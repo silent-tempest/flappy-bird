a9e08f73de89f7596789cf0fce581da630146174
silent-tempest <brussian768@gmail.com>
2018-03-04 15:00:13 +0700

    Implemented bird flying

--- scripts/main.js
+++ scripts/main.js
@@ -14,9 +14,10 @@
 
 var
   atan2 = Math.atan2,
+  ceil = Math.ceil,
   min = Math.min,
   max = Math.max,
-  PX_DENS = 0.8,
+  PX_DENS = 2,
   touchable = 'ontouchend' in window,
   pipes = [],
   MODE, safari, Bird, Pipe, game, bird, world;
@@ -25,7 +26,7 @@ safari = platform.os &&
   platform.os.family === 'iOS' &&
   platform.name === 'Safari';
 
-if ( false && touchable && !safari ) {
+if ( 0 && touchable && !safari ) {
   MODE = 'webgl';
 } else {
   MODE = '2d';
@@ -33,9 +34,10 @@ if ( false && touchable && !safari ) {
 
 world = {
   GRAVITY: v6.vec2( 0, 55 * PX_DENS ),
-  MIN_PIPE_HEIGHT: 256 * PX_DENS,
-  MAX_PIPE_HEIGHT: 384 * PX_DENS,
-  PIPE_OFF: 200 * PX_DENS,
+  MIN_PIPE_HEIGHT: 192 * PX_DENS,
+  MAX_PIPE_HEIGHT: 288 * PX_DENS,
+  MIN_PIPE_OFF: 200 * PX_DENS,
+  MAX_PIPE_OFF: 400 * PX_DENS,
   SPEED: 500 * PX_DENS
 };
 
@@ -55,15 +57,24 @@ game = {
 
   ui: {
     init: function init () {
+      this[ '#tip' ] = _( '#tip' );
       this[ '.theme' ] = _( '.theme' );
+      this[ '#score' ] = _( '#score' );
+      this[ '#results' ] = _( '#results' );
+      this[ '#results-score' ] = _( '#results-score' );
+      this[ '#restart-button' ] = _( '#restart-button' );
+    },
+
+    updateScore: function updateScore ( val ) {
+      if ( game.stopped ) {
+        this[ '#results-score' ].text( val );
+      } else {
+        this[ '#score' ].text( val );
+      }
     }
   },
 
   init: function init () {
-    var stopFly = function stopFly () {
-      game.touched = game.jumped = false;
-    };
-
     this.renderer = v6( {
       settings: {
         scale: PX_DENS
@@ -71,18 +82,17 @@ game = {
 
       mode: MODE
     } )
-      .lineWidth( 1.5 )
+      .lineWidth( 1.1 * PX_DENS )
       .noFill();
 
     bird = new Bird();
-    pipes.push( new Pipe() );
 
     this.camera = this.renderer.camera( {
       speed: [
         0.1, // x axis
         0.1, // y axis
-        0.01, // zoom in
-        0.01  // zoom out
+        0.05, // zoom in
+        0.05  // zoom out
       ],
 
       zoom: [
@@ -91,12 +101,20 @@ game = {
         1,  // current zoom
         1,  // min zoom (zoom out)
         1.5 // max zoom (zoom in)
-      ]
+      ],
+
+      linearZoom: {
+        zoomIn: true,
+        zoomOut: false
+      }
     } );
 
-    this.resize();
     this.ui.init();
-    this.changeColor();
+    this.resize();
+    this.stop();
+    this.restart();
+
+    pipes.push( new Pipe() );
 
     v6
       .ticker( this.update, this.draw, this )
@@ -105,16 +123,23 @@ game = {
     _( window )
       .resize( this.resize )
       .touchstart( this.fly )
-      .touchend( stopFly );
+      .touchend( this.stopFly );
   },
 
   update: function update ( dt ) {
+    var
+      last = _.last( pipes ),
+      i = this.COLLISION_STEPS,
+      j, xSpeed, ySpeed, len, off, pipe;
+
     if ( this.stopped ) {
       this.camera.zoomIn();
+    } else {
+      this.pipeOff = max( this.pipeOff - dt, world.MIN_PIPE_OFF );
     }
 
     if ( this.addSpeed ) {
-      this.pipeSpeed += world.SPEED * 0.02;
+      this.pipeSpeed += world.SPEED * 0.01;
 
       if ( this.pipeSpeed >= world.SPEED ) {
         this.pipeSpeed = world.SPEED;
@@ -135,11 +160,61 @@ game = {
 
     bird.update();
 
-    
+    xSpeed = this.pipeSpeed / i * dt;
+    ySpeed = bird.speed / i * dt;
+
+    for ( ; i > 0; --i ) {
+      bird.pos.y += ySpeed;
+
+      for ( j = pipes.length - 1; j >= 0; --j ) {
+        pipe = pipes[ j ];
+        pipe.x -= xSpeed;
+
+        if ( pipe.finished ) {
+          if ( pipe.top !== pipe.bottom ) {
+            pipe.top = min( pipe.top + 480 * dt * PX_DENS, pipe.bottom );
+            pipe.bottom = max( pipe.bottom - 480 * dt * PX_DENS, pipe.top );
+          }
+        } else if ( !pipe.finished && pipe.x + pipe.w < bird.pos.x - bird.r ) {
+          pipe.finished = true;
+          this.ui.updateScore( ++this.score );
+        }
+        
+        if ( pipe.x + pipe.w < -this.camera.offset.x / this.camera.zoom[ 1 ] ) {
+          pipes.splice( j, 1 );
+        } else if ( bird.hitsPipe( pipe ) ) {
+          for ( len = pipes.length; j < len; ++j ) {
+            pipes[ j ].x += xSpeed;
+          }
+
+          if ( bird.pos.y + bird.r > pipe.top && bird.pos.y - bird.r < pipe.bottom ) {
+            bird.pos.y -= ySpeed;
+          } else {
+            this.pipeSpeed = 0;
+          }
+
+          i = 0;
+          this.stop();
+          break;
+        }
+      }
+    }
+
+    while ( last ?
+      ( off = last.x + last.w + this.pipeOff ) < this.renderer.width / this.camera.zoom[ 1 ] :
+      !this.started )
+    {
+      last = this.addPipe( off );
+    }
 
     this.camera
       .lookAt( bird.pos )
       .update();
+
+    if ( this.score > this.highscore ) {
+      // todo save hs
+      // this.ui[ '#results-highscore' ].text( this.highscore = this.score );
+    }
   },
 
   draw: function draw () {
@@ -177,14 +252,57 @@ game = {
     }
   },
 
-  COLLISION_STEPS: 0,
+  stopFly: function stopFly () {
+    game.touched = game.flew = false;
+  },
+
+  stop: function stop () {
+    bird.speed = 0;
+    this.stopped = true;
+    this.started = this.addSpeed = false;
+    this.ui[ '#restart-button' ].show();
+    this.ui[ '#results' ].show();
+    this.ui[ '#score' ].hide();
+    this.ui.updateScore( this.score );
+  },
+
+  restart: function restart () {
+    bird.reset();
+    pipes.length = 0;
+    this.pipeOff = world.MAX_PIPE_OFF;
+    this.stopped = false;
+    this.zoomOut = true;
+    this.score = 0;
+    this.pipeSpeed = 0;
+    this.ui.updateScore( 0 );
+    this.ui[ '#restart-button' ].hide();
+    this.ui[ '#tip' ].show();
+    this.changeColor();
+  },
+
+  start: function start () {
+    this.ui[ '#results' ].hide();
+    this.ui[ '#tip' ].hide();
+    this.ui[ '#score' ].show();
+    this.started = this.addSpeed = true;
+  },
+
+  addPipe: function ( x ) {
+    var pipe = new Pipe( x );
+    pipes.push( pipe );
+    return pipe;
+  },
+
+  COLLISION_STEPS: ceil( PX_DENS ),
   pipeSpeed: 0,
-  collision: true,
+  pipeOff: world.MAX_PIPE_OFF,
+  highscore: 0,
+  score: 0,
   addSpeed: false,
   touched: false,
   started: false,
   stopped: true,
-  zoomOut: true,
+  zoomOut: false,
   flew: false
 };
 
@@ -192,9 +310,9 @@ Pipe = function Pipe ( x ) {
   var center, spacing;
 
   if ( x == null ) {
-    spacing = world.MIN_PIPE_HEIGHT;
+    spacing = world.MAX_PIPE_HEIGHT;
     center = Pipe.lastCenter = 0;
-    this.x = world.PIPE_OFF * 2;
+    this.x = world.MAX_PIPE_OFF;
   } else {
     spacing = _.random( world.MIN_PIPE_HEIGHT, world.MAX_PIPE_HEIGHT );
     center = Pipe.getCenter();
@@ -252,10 +370,15 @@ Bird = function Bird () {
   this.pos = v6.vec2();
   this.sides = 5;
   this.angle = 0;
-  this.spd = 0;
+  this.speed = 0;
 };
 
 Bird.prototype = {
+  reset: function reset () {
+    this.sides = _.random( 3, 5 );
+    this.speed = this.pos.y = 0;
+  },
+
   hitsPipe: function hitsPipe ( pipe ) {
     var
       pos = this.pos,
@@ -295,13 +418,13 @@ Bird.prototype = {
     return dx * dx + dy * dy <= r1 * r1;
   },
 
-  update: function ( dt ) {
-    this.spd = max( this.maxSpd, this.spd - world.GRAVITY );
-    this.angle += ( atan2( this.spd, game.pipeSpeed ) - this.angle ) * 0.1;
+  update: function update () {
+    this.speed = max( this.maxSpeed, this.speed + world.GRAVITY.y );
+    this.angle += ( atan2( this.speed, game.pipeSpeed ) - this.angle ) * 0.1;
   },
 
   fly: function fly () {
-    this.spd = max( this.maxSpd, this.spd + this.lift );
+    this.speed = max( this.maxSpeed, this.speed + this.lift );
   },
 
   show: function show () {
@@ -309,8 +432,8 @@ Bird.prototype = {
   },
 
   constructor: Bird,
-  maxSpd: -1200 * PX_DENS,
-  lift: 1750 * PX_DENS,
+  maxSpeed: -1200 * PX_DENS,
+  lift: -1750 * PX_DENS,
   r: 18 * PX_DENS
 };
 
